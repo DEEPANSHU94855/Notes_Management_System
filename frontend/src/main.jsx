5ede0fa8b6b4470c41defbdbd71f0ebaf80fe445
DEEPANSHU94855 <deepanshu98405@gmail.com>
2026-04-19 16:18:39 +0530

    Update React App to use Backend URL directly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
+
+// Set global base URL targeting the deployed backend
+axios.defaults.baseURL = 'https://notes-backend-pr3k.onrender.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
